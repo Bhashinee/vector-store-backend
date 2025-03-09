28676d620a0cc01d639de2426b0d04ed9963d1e6
lckariyawasam <lakinduk@wso2.com>
2025-03-09 16:17:57 +0530

    Implement Alternate parser
    Temporarily implementing a simple file parser with support only for
    pdf. Previous implementation is far better but currently raises
    an error in production. Simplifying the parser to check the user flow
    for now.

---
+++ backend/parser_alternate.py
@@ -0,0 +1,13 @@
+from pypdf import PdfReader
+
+# Simple Implementation
+def parse_content(file):
+    reader = PdfReader(file)
+    content = ""
+    for page in reader.pages:
+        content += page.extract_text()
+
+    print(content)
+    return content
+
+    
