76b651c5cf3474b13716db8cbbb0f9f6a263ac01
bhashinee <bhashineen@gmail.com>
2025-03-11 21:33:38 +0530

    Update the nltk download location

--- backend/chunker.py
+++ backend/chunker.py
@@ -6,7 +6,7 @@ nltk.data.path.append("/tmp/usr/local/share/nltk_data")
 try:
     nltk.data.find('tokenizers/punkt_tab')
 except LookupError:
-    nltk.download('punkt_tab', download_dir="/tmp/usr/local/share/nltk_data")
+    nltk.download('punkt_tab', download_dir="/tmp")
 
 def chunk_data(data: str, chunk_type: str, chunk_size: int = 100, max_chunk_size: int = 500, overlap: int = 0) -> List[str]:
     """
